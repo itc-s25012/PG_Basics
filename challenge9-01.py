0317897044177fd56a5a996077d0596601c18d65
itc-s25012 <s25012@std.it-college.ac.jp>
2025-06-18 01:35:55 +0000

    recommit PG_Basics

--- challenge9-01.py
+++
@@ -1,3 +0,0 @@
-with open("challenge.txt", "r", encoding="utf-8" ) as f:
-    print(f.read())
-
--- challenge9-02.py
+++
@@ -1,8 +0,0 @@
-text = input("あなたの好きな食べ物は?:")
-with open("food.txt", "w", encoding="utf-8") as f:
-    f.write(text)
-
-
-print("ファイルに書き込みました。")
-
-
